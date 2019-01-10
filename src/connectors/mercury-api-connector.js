6f26c41568c52fb8696abea1b20190db87b58ce2
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-01-10 21:35:19 +0200

    added mercury-api-connector

---
+++ src/connectors/mercury-api-connector.js
@@ -0,0 +1,17 @@
+import Axios from 'axios'
+
+export function fetchLoginOptions () {
+  return new Promise(function (resolve) {
+    resolve([{ text: 'scouting', value: 'scouting' }, { text: 'tactics', value: 'tactics' }])
+  })
+}
+
+export function login (loginType, password) {
+  return Axios.get(`/login/${loginType}`, {
+    params: {
+      password: password
+    }
+  }).then(function (response) {
+    return response.data
+  })
+}
