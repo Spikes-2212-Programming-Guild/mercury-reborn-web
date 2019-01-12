8c1632da046af84b0bf249e7ffab50a9cac2fca2
Ergoold <eran1gold@gmail.com>
2019-01-12 20:34:23 +0200

    added router to App.js and generic homepage component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from "react"
+import LoginView from "./views/LoginView"
+import Homepage from "./views/Homepage"
+import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom"
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      key: localStorage.getItem("key") || "-1"
+    }
+    this.updateKey = this.updateKey.bind(this)
+  }
+
+  updateKey(key) {
+    this.setState({key: key})
+  }
+
   render () {
-    return <div></div>
+    return <BrowserRouter>
+      <Switch>
+        <Route path="/" exact render={
+          () => {
+            switch(this.state.key) {
+            case "-1":
+              return <LoginView updateKey={this.updateKey} />
+            default:
+              return <Homepage />
+            }
+          }
+        } />
+      </Switch>
+    </BrowserRouter>
   }
 }
 
