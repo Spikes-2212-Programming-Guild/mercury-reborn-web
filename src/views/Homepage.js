f230556942acb8ac9cfdf9f7e778305761718663
Ergoold <eran1gold@gmail.com>
2019-01-13 21:33:01 +0200

    added Enum to Homepage for testing purposes

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Text from "../components/questions/text"
 import Number from "../components/questions/number"
+import Enum from "../components/questions/enum"
 
 class Homepage extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class Homepage extends React.Component {
         <h1>Homepage</h1>
         <Text />
         <Number min={1} />
+        <Enum options={["uno", "dos", "tres"]} selected={"uno"} />
       </div>
     )
   }
