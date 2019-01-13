765ffb9a073634cf2a043187dac6b1fdac4364a8
Ergoold <eran1gold@gmail.com>
2019-01-13 21:38:41 +0200

    added Boolean to Homepage for testing purposes

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -2,6 +2,7 @@ import React from "react"
 import Text from "../components/questions/text"
 import Number from "../components/questions/number"
 import Enum from "../components/questions/enum"
+import Boolean from "../components/questions/boolean"
 
 class Homepage extends React.Component {
   render() {
@@ -10,7 +11,8 @@ class Homepage extends React.Component {
         <h1>Homepage</h1>
         <Text />
         <Number min={1} />
-        <Enum options={["uno", "dos", "tres"]} selected={"uno"} />
+        <Enum options={["uno", "dos", "tres"]} selected="tres" />
+        <Boolean selected="true" />
       </div>
     )
   }
