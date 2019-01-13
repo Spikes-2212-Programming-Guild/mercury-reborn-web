385b97734a010b63a8443cc4084e587137d049a5
Ergoold <eran1gold@gmail.com>
2019-01-13 21:20:02 +0200

    added Number to Homepage for testing purposes

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Text from "../components/questions/text"
+import Number from "../components/questions/number"
 
 class Homepage extends React.Component {
   render() {
@@ -7,6 +8,7 @@ class Homepage extends React.Component {
       <div>
         <h1>Homepage</h1>
         <Text />
+        <Number min={1} />
       </div>
     )
   }
