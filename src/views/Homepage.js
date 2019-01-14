22a949f2f1cd0c79b819ad2befba412c6898b7fa
Ergoold <eran1gold@gmail.com>
2019-01-14 20:14:17 +0200

    cleared components added for testing purposes from Homepage

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,17 +1,9 @@
 import React from "react"
-import Text from "../components/questions/text"
-import Number from "../components/questions/number"
-import Enum from "../components/questions/enum"
-import Boolean from "../components/questions/boolean"
 
 class Homepage extends React.Component {
   render() {
     return (<div>
       <h1>Homepage</h1>
-      <Text />
-      <Number min={1} />
-      <Enum options={["uno", "dos", "tres"]} selected="tres" />
-      <Boolean selected="true" />
     </div>)
   }
 }
