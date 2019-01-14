41e47244dbf4018fd6746780f0967427451d7948
Ergoold <eran1gold@gmail.com>
2019-01-14 20:11:46 +0200

    added prop to turn helpers on and of

--- src/components/questions/number.js
+++ src/components/questions/number.js
@@ -1,6 +1,6 @@
 import React from "react"
 import propTypes from "prop-types"
-import {Button, Label} from "semantic-ui-react"
+import {Button, Label, Input} from "semantic-ui-react"
 
 class Number extends React.Component {
   constructor(props) {
@@ -8,6 +8,7 @@ class Number extends React.Component {
     this.state = {
       min: this.props.min,
       num: this.props.min,
+      helpers: this.props.helpers,
     }
   }
 
@@ -20,18 +21,23 @@ class Number extends React.Component {
     this.setState({num: this.state.num + 1})
   }
 
+  handleChange(e, data) {
+    this.setState({num: Number(data.value)})
+  }
+
   render() {
     return (<div>
-      <Button attached="left" onClick={this.handleMinusClick.bind(this)}>-</Button>
-      <Label>{this.state.num}</Label>
-      <Button attached="right" onClick={this.handlePlusClick.bind(this)}>+</Button>
+      {this.state.helpers ? <Button attached="left" onClick={this.handleMinusClick.bind(this)}>-</Button> : ""}
+      {this.state.helpers ? <Label>{this.state.num}</Label> : <Input onChange={this.handleChange.bind(this)} />}
+      {this.state.helpers ? <Button attached="right" onClick={this.handlePlusClick.bind(this)}>+</Button> : ""}
     </div>)
   }
 }
 
 Number.propTypes = {
   min: propTypes.number,
-  num: propTypes.number
+  num: propTypes.number,
+  helpers: propTypes.bool
 }
 
 export default Number
