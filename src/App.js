f253f33609ccd174dfae44466120aeff251f52c7
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-01-20 12:16:27 +0200

    added router to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from "react"
+import LoginView from "./views/LoginView"
+import Homepage from "./views/Homepage"
+import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom"
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      key: localStorage.getItem("key") || null
+    }
+  }
+
+  updateKey(key) {
+    this.setState({key})
+  }
+
   render () {
-    return <div></div>
+    return <BrowserRouter>
+      <Switch>
+        <Route path="/" exact render={
+          () => {
+            if(this.state.key) {
+              return <Homepage />
+            } else {
+              return <LoginView updateKey={this.updateKey.bind(this)} />
+            }
+          }
+        } />
+      </Switch>
+    </BrowserRouter>
   }
 }
 
