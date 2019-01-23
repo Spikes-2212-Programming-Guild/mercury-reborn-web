eddd168f96d3eeabf8def0fcfe302aa0c8811c8b
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-01-23 17:48:14 +0200

    made functions in tba connector async

--- src/connectors/mercury-api-connector.js
+++ src/connectors/mercury-api-connector.js
@@ -1,17 +1,15 @@
 import axios from "axios"
 
-export function fetchLoginOptions () {
-  return new Promise(function (resolve) {
-    resolve([{text: "scouting", value: "scouting"}, {text: "tactics", value: "tactics"}])
-  })
+export async function fetchLoginOptions () {
+  return [{text: "scouting", value: "scouting"}, {text: "tactics", value: "tactics"}]
 }
 
-export function login (loginType, password) {
-  return axios.get(`/login/${loginType}`, {
+export async function login (loginType, password) {
+  return (await axios.get(`/login/${loginType}`, {
     params: {
       password: password
     }
-  }).then(response => response.data)
+  })).data
 }
 
 export function fetchTBAKey() {
