4b61773f0a0a3ab463e9ee05f907e5fd036c8015
Ergoold <eran1gold@gmail.com>
2019-01-23 22:32:34 +0200

    made form container fetch scouting form from server

--- src/containers/form-container.js
+++ src/containers/form-container.js
@@ -1,7 +1,11 @@
 import {Container} from "unstated"
+import {fetchScoutingForm} from "../connectors/mercury-api-connector";
 
 class FormContainer extends Container {
-
+  constructor() {
+    super()
+    this.state = fetchScoutingForm()
+  }
 }
 
 export default FormContainer
