9bb3ff4893b0db820b382523df013c7b6c64a4b2
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-01-23 20:09:33 +0200

    made tba-connector sort matches by comp_lvl

--- src/connectors/tba-api-connector.js
+++ src/connectors/tba-api-connector.js
@@ -4,6 +4,13 @@ import * as mercuryAPI from "./mercury-api-connector"
 
 const tbaAddress = "https://www.thebluealliance.com/api/v3"
 
+const compLevelOrder = {
+  qm: 0,
+  qf: 1,
+  sf: 2,
+  f: 3
+}
+
 export async function checkStatus() {
   const tbaKey = await fetchTBAKey()
 
@@ -26,8 +33,16 @@ export async function fetchGamesForEvent() {
   })).data
 
   return result.map(game => ({
+    number: game.match_number,
     blue: game.alliances.blue.team_keys,
     red: game.alliances.red.team_keys,
     comp_level: game.comp_level
-  }))
+  })).sort((a, b) => {
+    a = compLevelOrder[a.comp_level]
+    b = compLevelOrder[b.comp_level]
+
+    if (a > b) return 1
+    if (a < b) return -1
+    return 0
+  })
 }
