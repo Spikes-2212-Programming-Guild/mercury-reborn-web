23bd344511b3d32356b3c732cc629136ed6668e8
Ergoold <eran1gold@gmail.com>
2019-01-24 18:40:13 +0200

    ScoutingForm now fetches scouting form and initiates a form container with it

--- src/components/ScoutingForm.js
+++ src/components/ScoutingForm.js
@@ -2,15 +2,25 @@ import React from "react"
 import QuestionPage from "./QuestionPage"
 import FormContainer from "../containers/form-container"
 import {Provider} from "unstated"
+import {fetchScoutingForm} from "../connectors/mercury-api-connector";
 
 class ScoutingForm extends React.Component {
   constructor(props) {
     super(props)
-    this.form = new FormContainer
+    this.container = new FormContainer()
+
+    this.state = {
+      form: {}
+    }
+
+    fetchScoutingForm().then(form => {
+      this.container.initialize(form)
+      this.setState({form})
+    })
   }
 
   render() {
-    return <Provider inject={[this.form]}>
+    return <Provider inject={[this.container]}>
 
     </Provider>
   }
