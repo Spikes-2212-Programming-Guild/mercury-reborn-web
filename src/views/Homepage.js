6f4400ac754f8073b5c622ae4ade07f176f2f93b
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-09 19:24:29 +0200

    made Homepage enter scouting view

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,10 +1,17 @@
 import React from "react"
 
+import {Link} from "react-router-dom"
+import {Label} from "semantic-ui-react"
+
 class Homepage extends React.Component {
   render() {
-    return (<div>
-      <h1>Homepage</h1>
-    </div>)
+    return (
+      <div>
+        <h1>Homepage</h1>
+
+        <Label size={"massive"}><Link to={"/scouting-form/matches"}>Scout!</Link></Label>
+      </div>
+    )
   }
 }
 
