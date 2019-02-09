0745668ae3343682ca94baf7791304669e48ca5a
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-09 19:24:53 +0200

    made app initialize TBA connection

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react"
 import Homepage from "./views/Homepage"
-import {BrowserRouter, Route, Switch} from "react-router-dom"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import {Provider} from "unstated"
+import ScoutingMenu from "./views/ScoutingMenu"
+import {initializeConnection} from "./connectors/tba-api/connector"
 
 class App extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
+
+    initializeConnection()
   }
 
   render () {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact render={Homepage}/>
+          <Route path="/scouting-form" component={ScoutingMenu}/>
+          <Route exact path="/" component={Homepage}/>
         </Switch>
       </BrowserRouter>
     )
