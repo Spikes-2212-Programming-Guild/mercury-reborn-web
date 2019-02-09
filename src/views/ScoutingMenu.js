0ed44645b16a7e52b45186c98afc3038021f9ba9
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-09 19:24:07 +0200

    completed ScoutingMenu

--- src/views/ScoutingMenu.js
+++ src/views/ScoutingMenu.js
@@ -1,19 +1,38 @@
 import React from "react"
+import {Subscribe, Provider} from "unstated"
+import {MatchList} from "../components/match-list"
+import {Route} from "react-router-dom"
+import ScoutingMenuContainer from "../containers/scouting-menu-container"
+import Index from "../components/scouting-menu/match/index"
 
-import {fetchMatchesForScoutingMenu} from "../connectors/tba-api/connector"
-import {Label} from "semantic-ui-react"
-import {Switch} from "react-router-dom"
 
-function MatchItem(props) {
-  return <Label title={props.title}></Label>
+function MatchesMenu(props) {
+  return (
+    <Subscribe to={[ScoutingMenuContainer]}>
+      {container => {
+        console.log("matches")
+        return <MatchList matches={container.state.matches}/>
+      }}
+    </Subscribe>
+  )
 }
 
-function ScoutingMenu(props) {
-  return (
-    <div>
 
-    </div>
-  )
+class ScoutingMenu extends React.Component {
+  constructor(props) {
+    super(props)
+    this.container = new ScoutingMenuContainer()
+    this.container.fetchMatches()
+  }
+
+  render() {
+    return (
+      <Provider inject={[this.container]}>
+        <Route exact path={`${this.props.match.path}/matches`} component={MatchesMenu}/>
+        <Route path={`${this.props.match.path}/matches/:name`} component={Index}/>
+      </Provider>
+    )
+  }
 }
 
 export default ScoutingMenu
