11711504fa1592e7f0d4f0a7266b500920f1905f
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-09 10:57:10 +0200

    added scouting-menu-container

---
+++ src/containers/scouting-menu-container.js
@@ -0,0 +1,35 @@
+import {Container} from "unstated"
+import { fetchMatchesForScoutingMenu } from "../connectors/tba-api/connector"
+
+class ScoutingMenuContainer extends Container {
+
+  constructor () {
+    super()
+    this.state = {
+      matches: null
+    }
+  }
+
+  /**
+   * fetches all the matches from the tba api
+   */
+  fetchMatches() {
+    fetchMatchesForScoutingMenu().then(matches => {
+      if (!this.state.matches) {
+        return this.setState({matches})
+      }
+    })
+  }
+
+  /**
+   * finds the match with the given name
+   * @param matchName {string} the name of the target match
+   * @returns {*} the match
+   */
+  getMatch(matchName) {
+    return this.state.matches.filter(match => (match.comp_level + match.number) === matchName)[0]
+  }
+
+}
+
+export default ScoutingMenuContainer
