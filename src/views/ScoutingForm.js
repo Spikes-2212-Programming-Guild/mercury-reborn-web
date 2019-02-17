04e781e912f457fe92c0b88b551c054680b44068
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-17 17:37:18 +0200

    fixed form generation

--- src/views/ScoutingForm.js
+++ src/views/ScoutingForm.js
@@ -4,14 +4,18 @@ import ScoutingFormContainer from "../containers/scouting-form-container"
 import {Provider} from "unstated"
 import {fetchScoutingForm} from "../connectors/mercury-api-connector"
 import * as _ from "lodash"
+import {Form, Header, Container, Grid} from "semantic-ui-react"
 
 class ScoutingForm extends React.Component {
   constructor(props) {
     super(props)
     this.container = new ScoutingFormContainer()
 
+    const {name, team} = this.props.match.params
+
     this.state = {
-      form: {}
+      form: {},
+      name, team
     }
 
     fetchScoutingForm().then(form => {
@@ -23,15 +27,23 @@ class ScoutingForm extends React.Component {
 
   render() {
     return (
-      <Provider inject={[this.container]}>
-        {
-          _.map(this.state.form, (questions, section) => (
-            <QuestionPage
-              questions={questions}
-              set={(value, questionName) => this.container.set(section, questionName, value)}/>
-          ))
-        }
-      </Provider>
+      <div className="scoutingForm segment centered">
+        <Header as="h1">{this.state.team}</Header>
+        <Header dividing as="h2">Scouting</Header>
+        <Provider inject={[this.container]}>
+          <Form>
+            {
+              _.map(this.state.form, (questions, section) => (
+                <QuestionPage
+                  questions={questions}
+                  key={section}
+                  title={section}
+                  set={(value, questionName) => this.container.set(section, questionName, value)}/>
+              ))
+            }
+          </Form>
+        </Provider>
+      </div>
     )
   }
 }
