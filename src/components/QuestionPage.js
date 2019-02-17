05c7171fcfd4279cb802ca62c9c4d6ca47059044
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-17 17:37:51 +0200

    made QuestionPage items form items

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -4,18 +4,17 @@ import Enum from "./questions/enum"
 import Boolean from "./questions/boolean"
 import Number from "./questions/number/"
 import Text from "./questions/text"
-import {Form, Label} from "semantic-ui-react"
-
+import {Form, Header} from "semantic-ui-react"
 
 const QuestionRegistry = {
   "number": (props, key, valueConsumer) => (<Number {...props} valueConsumer={valueConsumer} key={key}/>),
-  "enum": (props, key, valueConsumer) => (<Enum {...props} valueConsumer={valueConsumer}  key={key}/>),
-  "text": (props, key, valueConsumer) => (<Text {...props} valueConsumer={valueConsumer}  key={key}/>),
-  "boolean": (props, key, valueConsumer) => (<Boolean {...props} valueConsumer={valueConsumer}  key={key}/>)
+  "enum": (props, key, valueConsumer) => (<Enum {...props} valueConsumer={valueConsumer} key={key}/>),
+  "text": (props, key, valueConsumer) => (<Text {...props} valueConsumer={valueConsumer} key={key}/>),
+  "boolean": (props, key, valueConsumer) => (<Boolean {...props} valueConsumer={valueConsumer} key={key}/>)
 }
 
 class QuestionPage extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
   }
 
@@ -24,17 +23,16 @@ class QuestionPage extends React.Component {
     set: propTypes.func
   }
 
-  render() {
+  render () {
     return (
       <div>
-        <Form>
-          {this.props.questions.map((question, index) => (
-            <div>
-              <Label>{question.name}</Label>
-              <div>{QuestionRegistry[question.type](question, index, answer => this.props.set(answer, question.name))}</div>
-            </div>
-          ))}
-        </Form>
+        <Header as="h3" dividing>{this.props.title}</Header>
+        {this.props.questions.map((question, index) => (
+          <Form.Field key={index}>
+            <Header as="h5">{question.name}</Header>
+            {QuestionRegistry[question.type](question, index, answer => this.props.set(answer, question.name))}
+          </Form.Field>
+        ))}
       </div>
     )
   }
