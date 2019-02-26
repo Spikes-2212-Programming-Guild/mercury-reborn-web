c25d90528134708595455ea4809d8e03343eb4e0
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-26 15:17:02 +0200

    made tba-api connector use config-manager

--- src/connectors/tba-api/connector.js
+++ src/connectors/tba-api/connector.js
@@ -1,13 +1,14 @@
 import axios from "axios"
 import * as mercuryAPI from "../mercury-api-connector"
 import {filterScoutingMenuProperties, sortMatchesByCompLevel} from "./data-filters"
+import * as configManager from "../../util/config-manager"
 
 const tbaAddress = "https://www.thebluealliance.com/api/v3"
 
 
 
 export async function initializeConnection() {
-  axios.defaults.headers.common["X-TBA-Auth-Key"] = await mercuryAPI.fetchTBAKey()
+  axios.defaults.headers.common["X-TBA-Auth-Key"] = configManager.getTBAKey()
 }
 
 export async function checkStatus() {
@@ -20,7 +21,7 @@ async function fetchMatchesForEvent(eventKey) {
 }
 
 export async function fetchMatchesForScoutingMenu() {
-  const eventKey = await mercuryAPI.fetchEventKey()
+  const eventKey = await configManager.getEventKey()
   let matches = await fetchMatchesForEvent(eventKey)
   matches = filterScoutingMenuProperties(matches)
   matches = sortMatchesByCompLevel(matches)
