9273a40b0c83d2d006d3426c5ecf9a7f84872a21
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-26 14:52:36 +0200

    fixed redirect in match

--- src/components/scouting-menu/match/match.js
+++ src/components/scouting-menu/match/match.js
@@ -2,60 +2,70 @@ import React from "react"
 import {Grid, Button} from "semantic-ui-react"
 import {Subscribe} from "unstated"
 import ScoutingMenuContainer from "../../../containers/scouting-menu-container"
+import {withRouter} from "react-router-dom"
 import "./style.css"
 
-function scoutTeam(matchName, team) {
-  const newPath = `/${window.location.pathname.split("/")[1]}/matches/scout/${matchName}/${team}`
-  window.location.pathname = newPath
-}
 
-function Match(props) {
-  const {match} = props
-  const {name}  = match.params
-  return (
-    <Subscribe to={[ScoutingMenuContainer]}>
-      {container => {
-        const matchData = container.getMatch(name)
-        if (matchData) {
-          const {blue, red} = matchData
+class Match extends React.Component {
 
-          const joined = []
-          for (let i = 0; i < blue.length; i++) {
-            joined.push({blue: blue[i], red:red[i]});
+  constructor (props) {
+    super(props)
+  }
+
+  scoutTeam(matchName, team) {
+    const newPath = `/${window.location.pathname.split("/")[1]}/matches/scout/${matchName}/${team}`
+    this.props.history.push(newPath)
+  }
+
+
+  render() {
+    const {match} = this.props
+    const {name} = match.params
+    return (
+      <Subscribe to={[ScoutingMenuContainer]}>
+        {container => {
+          const matchData = container.getMatch(name)
+          if (matchData) {
+            const {blue, red} = matchData
+
+            const joined = []
+            for (let i = 0; i < blue.length; i++) {
+              joined.push({blue: blue[i], red: red[i]});
+            }
+            return (
+              <div>
+                <Grid centered padded columns={2}>
+                  <Grid.Column>
+                    <Button.Group color={"blue"} inverted vertical fluid>
+                      {blue.map(team => (
+                        <Button
+                          class_name="team_button"
+                          onClick={() => this.scoutTeam(name, team)}>
+                          {team}
+                        </Button>
+                      ))}
+                    </Button.Group>
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Button.Group color={"red"} inverted vertical fluid>
+                      {red.map(team => (
+                        <Button
+                          class_name="team_button"
+                          onClick={() => this.scoutTeam(name, team)}>
+                          {team}
+                        </Button>
+                      ))}
+                    </Button.Group>
+                  </Grid.Column>
+                </Grid>
+              </div>
+            )
           }
-          return (
-            <div >
-              <Grid centered padded columns={2}>
-                <Grid.Column>
-                  <Button.Group color={"blue"} inverted vertical fluid >
-                    {blue.map(team => (
-                      <Button
-                        class_name="team_button"
-                        onClick={() => scoutTeam(name, team)}>
-                        {team}
-                      </Button>
-                    ))}
-                  </Button.Group>
-                </Grid.Column>
-                <Grid.Column>
-                  <Button.Group color={"red"} inverted vertical fluid >
-                    {red.map(team => (
-                      <Button
-                        class_name="team_button"
-                        onClick={() => scoutTeam(name, team)}>
-                        {team}
-                      </Button>
-                    ))}
-                  </Button.Group>
-                </Grid.Column>
-              </Grid>
-            </div>
-          )
-        }
-        return ""
-      }}
-    </Subscribe>
-  )
+          return ""
+        }}
+      </Subscribe>
+    )
+  }
 }
 
-export default Match
+export default withRouter(Match)
