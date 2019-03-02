507f0d4d281a28c970a5cc83046d11067b899a94
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-02 16:57:37 +0200

    fixed scouting menu

--- src/views/ScoutingMenu.js
+++ src/views/ScoutingMenu.js
@@ -32,7 +32,6 @@ function TeamsMenu (props) {
   )
 }
 
-
 class ScoutingMenu extends React.Component {
   constructor (props) {
     super(props)
@@ -44,13 +43,45 @@ class ScoutingMenu extends React.Component {
   render () {
     return (
       <Provider inject={[this.container]}>
-        <Route exact path={`${this.props.match.path}/field/matches`} component={() => <MatchesMenu parentURL={`${this.props.match.path}/field`}/>}/>
-        <Route path={`${this.props.match.path}/field/matches/:name`} component={() => <Match parentURL={`${this.props.match.path}/field/matches/scout/`}/>}/>
-        <Route path={`${this.props.match.path}/field/matches/scout/:name/:team/`} component={(props) => <ScoutingForm {...props} formPromise={configManager.getFieldForm()} title={`Scouting - ${props.match.params.team.replace("frc", "")}`}/>}/>
+        <Route
+          exact
+          path={`${this.props.match.path}/field/matches`}
+          component={() => <MatchesMenu parentURL={`${this.props.match.path}/field`}/>}/>
+        <Route
+          path={`${this.props.match.path}/field/matches/:name`}
+          component={() => <Match parentURL={`${this.props.match.path}/field/matches/scout/`}/>}/>
+        <Route
+          path={`${this.props.match.path}/field/matches/scout/:name/:team/`}
+          component={(props) => (
+            <ScoutingForm
+              {...props}
+              formPromise={configManager.getFieldForm()}
+              formConsumer={submitFieldForm}
+              title={`Scouting - ${props.match.params.team.replace("frc", "")}`}/>
+          )}/>
         <Route exact path={`${this.props.match.path}/pit/teams`} component={(props) => <TeamsMenu {...props}/>}/>
-        <Route exact path={`${this.props.match.path}/viewer/matches`} component={props => <MatchesMenu parentURL={`${this.props.match.path}/viewer`}/>} />
-        <Route exact path={`${this.props.match.path}/pit/:team`} component={(props) => <ScoutingForm {...props} formPromise={configManager.getPitForm()} title={`Pit - ${props.match.params.team}`}/>}/>
-        <Route exact path={`${this.props.match.path}/viewer/matches/:match`} component={props => <ScoutingForm  {...props} formPromise={configManager.getViewerForm()} title={`Match - ${props.match.params.match}`}/>} />
+        <Route
+          exact
+          path={`${this.props.match.path}/viewer/matches`}
+          component={() => <MatchesMenu parentURL={`${this.props.match.path}/viewer`}/>}/>
+        <Route
+          exact
+          path={`${this.props.match.path}/pit/:team`}
+          component={(props) => (
+            <ScoutingForm
+              {...props}
+              formPromise={configManager.getPitForm()}
+              title={`Pit - ${props.match.params.team}`}/>
+          )}/>
+        <Route
+          exact
+          path={`${this.props.match.path}/viewer/matches/:match`}
+          component={props => (
+            <ScoutingForm
+              {...props}
+              formPromise={configManager.getViewerForm()}
+              title={`Match - ${props.match.params.match}`}/>
+          )}/>
       </Provider>
     )
   }
