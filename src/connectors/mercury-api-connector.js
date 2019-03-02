6a9b7d132c795b9549fe64170061ee892a90d0ac
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-02 16:48:28 +0200

    made mercury-api submit three forms

--- src/connectors/mercury-api-connector.js
+++ src/connectors/mercury-api-connector.js
@@ -9,6 +9,14 @@ export const fetchEventKey = () => fetchConfigOption("event-name")
 export const fetchScoutingForm = () => fetchConfigOption("form")
 
 
-export async function submitMatch (teamNumber, match) {
-  return await axios.post("/match/submit", {teamNumber, match})
+export async function submitFieldForm (match) {
+  return await axios.post("/scouting/field/match/submit", {match})
+}
+
+export async function submitPitForm(form) {
+  return await axios.post("/scouting/pit/team/submit", {form})
+}
+
+export async function submitViewerForm(match) {
+  return await axios.post("/scouting/viewer/match/submit", {match})
 }
