03afa9f543ea1dae220efe8b08161a03aa926d9c
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-02 16:47:37 +0200

    made scoutingForm use formConsumer

--- src/views/ScoutingMenu.js
+++ src/views/ScoutingMenu.js
@@ -1,7 +1,7 @@
 import React from "react"
-import {Subscribe, Provider} from "unstated"
-import {MatchList} from "../components/match-list"
-import {Route} from "react-router-dom"
+import { Subscribe, Provider } from "unstated"
+import { MatchList } from "../components/match-list"
+import { Route } from "react-router-dom"
 import ScoutingMenuContainer from "../containers/scouting-menu-container"
 import Match from "../components/scouting-menu/match/match"
 import FormViewer from "./ViewerForm"
@@ -9,9 +9,9 @@ import ScoutingForm from "../components/ScoutingForm"
 import * as tbaApi from "../connectors/tba-api/connector"
 import * as configManager from "../util/config-manager"
 import TeamsList from "../components/teams-list"
+import { submitFieldForm } from "../connectors/mercury-api-connector"
 
-
-function MatchesMenu(props) {
+function MatchesMenu (props) {
   return (
     <Subscribe to={[ScoutingMenuContainer]}>
       {container => {
@@ -22,7 +22,7 @@ function MatchesMenu(props) {
   )
 }
 
-function TeamsMenu(props) {
+function TeamsMenu (props) {
   return (
     <Subscribe to={[ScoutingMenuContainer]}>
       {container => {
@@ -32,21 +32,33 @@ function TeamsMenu(props) {
   )
 }
 
-
 class ScoutingMenu extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.container = new ScoutingMenuContainer()
     tbaApi.fetchMatchesForScoutingMenu().then(matches => this.container.setMatches(matches))
     tbaApi.fetchTeamsForTeamsList().then(teams => this.container.setTeams(teams))
   }
 
-  render() {
+  render () {
     return (
       <Provider inject={[this.container]}>
-        <Route exact path={`${this.props.match.path}/field/matches`} component={() => <MatchesMenu parentURL={`${this.props.match.path}/field`}/>}/>
-        <Route path={`${this.props.match.path}/field/matches/:name`} component={() => <Match parentURL={`${this.props.match.path}/field/matches/scout/`}/>}/>
-        <Route path={`${this.props.match.path}/field/matches/scout/:name/:team/`} component={(props) => <ScoutingForm {...props} formPromise={configManager.getScoutingForm()}/>}/>
+        <Route
+          exact
+          path={`${this.props.match.path}/field/matches`}
+          component={() => <MatchesMenu parentURL={`${this.props.match.path}/field`}/>}/>
+        <Route
+          path={`${this.props.match.path}/field/matches/:name`}
+          component={() => <Match parentURL={`${this.props.match.path}/field/matches/scout/`}/>}/>
+        <Route
+          path={`${this.props.match.path}/field/matches/scout/:name/:team/`}
+          component={(props) => (
+            <ScoutingForm
+              {...props}
+              formConsumer={submitFieldForm}
+              formPromise={configManager.getScoutingForm()}/>
+          )}
+        />
         <Route exact path={`${this.props.match.path}/pit/teams`} component={(props) => <TeamsMenu {...props}/>}/>
       </Provider>
     )
