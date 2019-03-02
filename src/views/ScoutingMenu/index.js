503764d02aa7a3c459bc999d93267c9e0e7525df
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-02 17:12:10 +0200

    separated ScoutingMenu into a separated directory

---
+++ src/views/ScoutingMenu/index.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Provider } from "unstated"
+import ScoutingMenuContainer from "../../containers/scouting-menu-container"
+import * as tbaApi from "../../connectors/tba-api/connector"
+
+import {FieldRoutes, PitRoutes, ViewerRoutes} from "./routes"
+
+export default class ScoutingMenu extends React.Component {
+  constructor (props) {
+    super(props)
+    this.container = new ScoutingMenuContainer()
+    tbaApi.fetchMatchesForScoutingMenu().then(matches => this.container.setMatches(matches))
+    tbaApi.fetchTeamsForTeamsList().then(teams => this.container.setTeams(teams))
+  }
+
+  render () {
+    return (
+      <Provider inject={[this.container]}>
+        <ViewerRoutes/>
+        <PitRoutes/>
+        <FieldRoutes/>
+      </Provider>
+    )
+  }
+}
