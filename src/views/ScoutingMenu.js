d5b1b7cca345c426249aefb247eaaf5caf6acdb5
suerflowz <moshesher1998@gmail.com>
2019-03-02 14:59:02 +0200

    display team number in title

--- src/views/ScoutingMenu.js
+++ src/views/ScoutingMenu.js
@@ -46,9 +46,9 @@ class ScoutingMenu extends React.Component {
       <Provider inject={[this.container]}>
         <Route exact path={`${this.props.match.path}/field/matches`} component={() => <MatchesMenu parentURL={`${this.props.match.path}/field`}/>}/>
         <Route path={`${this.props.match.path}/field/matches/:name`} component={() => <Match parentURL={`${this.props.match.path}/field/matches/scout/`}/>}/>
-        <Route path={`${this.props.match.path}/field/matches/scout/:name/:team/`} component={(props) => <ScoutingForm {...props} formPromise={configManager.getScoutingForm()} title="Scouting"/>}/>
+        <Route path={`${this.props.match.path}/field/matches/scout/:name/:team/`} component={(props) => <ScoutingForm {...props} formPromise={configManager.getScoutingForm()} title={`Scouting - ${props.match.params.team.replace("frc", "")}`}/>}/>
         <Route exact path={`${this.props.match.path}/pit/teams`} component={(props) => <TeamsMenu {...props}/>}/>
-        <Route exact path={`${this.props.match.path}/pit/:team`} component={(props) => <ScoutingForm {...props} formPromise={configManager.getPitForm()} title="Pit Scouting"/>}/>
+        <Route exact path={`${this.props.match.path}/pit/:team`} component={(props) => <ScoutingForm {...props} formPromise={configManager.getPitForm()} title={`Pit - ${props.match.params.team}`}/>}/>
       </Provider>
     )
   }
