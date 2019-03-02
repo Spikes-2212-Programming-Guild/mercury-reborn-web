fba3f77a9cbcaa4abfa5e1e791fd3ed7961564bc
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-02 12:08:22 +0200

    made scouting form connector fetch teams

--- src/connectors/tba-api/connector.js
+++ src/connectors/tba-api/connector.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 import * as mercuryAPI from "../mercury-api-connector"
-import {filterScoutingMenuProperties, sortMatchesByCompLevel} from "./data-filters"
+import {filterScoutingMenuProperties, sortMatchesByCompLevel, filterTeamProperties} from "./data-filters"
 import * as configManager from "../../util/config-manager"
 
 const tbaAddress = "https://www.thebluealliance.com/api/v3"
@@ -20,10 +20,20 @@ async function fetchMatchesForEvent(eventKey) {
   return (await axios.get(`${tbaAddress}/event/${eventKey}/matches/simple`)).data
 }
 
+async function fetchTeamsForEvent(eventKey) {
+  return await axios.get(`${tbaAddress}/event/${eventKey}/teams`)
+}
+
 export async function fetchMatchesForScoutingMenu() {
   const eventKey = await configManager.getEventKey()
   let matches = await fetchMatchesForEvent(eventKey)
   matches = filterScoutingMenuProperties(matches)
   matches = sortMatchesByCompLevel(matches)
   return matches
+}
+
+export async function fetchTeamsForTeamsList() {
+  const eventKey = await configManager.getEventKey()
+  let teams = await fetchTeamsForEvent(eventKey)
+  return filterTeamProperties(teams)
 }
