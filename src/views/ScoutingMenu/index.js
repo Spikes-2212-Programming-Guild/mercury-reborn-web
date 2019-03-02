4cd15f37290e01414ee499218c871c987d0b1527
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-02 17:30:17 +0200

    added container to state

--- src/views/ScoutingMenu/index.js
+++ src/views/ScoutingMenu/index.js
@@ -8,14 +8,15 @@ import {FieldRoutes, PitRoutes, ViewerRoutes} from "./routes"
 export default class ScoutingMenu extends React.Component {
   constructor (props) {
     super(props)
-    this.container = new ScoutingMenuContainer()
-    tbaApi.fetchMatchesForScoutingMenu().then(matches => this.container.setMatches(matches))
-    tbaApi.fetchTeamsForTeamsList().then(teams => this.container.setTeams(teams))
+    this.state = {}
+    this.state.container = new ScoutingMenuContainer()
+    tbaApi.fetchMatchesForScoutingMenu().then(matches => this.state.container.setMatches(matches))
+    tbaApi.fetchTeamsForTeamsList().then(teams => this.state.container.setTeams(teams))
   }
 
   render () {
     return (
-      <Provider inject={[this.container]}>
+      <Provider inject={[this.state.container]}>
         <ViewerRoutes {...this.props}/>
         <PitRoutes {...this.props}/>
         <FieldRoutes {...this.props}/>
