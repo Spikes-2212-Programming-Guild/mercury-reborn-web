6b8f4cc232964454cc285e1b2d9a9c00902a108c
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-02 18:19:54 +0200

    fixes

--- src/views/ScoutingMenu/index.js
+++ src/views/ScoutingMenu/index.js
@@ -3,7 +3,7 @@ import { Provider } from "unstated"
 import ScoutingMenuContainer from "../../containers/scouting-menu-container"
 import * as tbaApi from "../../connectors/tba-api/connector"
 
-import {FieldRoutes, PitRoutes, ViewerRoutes} from "./routes"
+import {FieldRoutes, PitRoutes, SpectatorRoutes} from "./routes"
 
 export default class ScoutingMenu extends React.Component {
   constructor (props) {
@@ -17,7 +17,7 @@ export default class ScoutingMenu extends React.Component {
   render () {
     return (
       <Provider inject={[this.state.container]}>
-        <ViewerRoutes {...this.props}/>
+        <SpectatorRoutes {...this.props}/>
         <PitRoutes {...this.props}/>
         <FieldRoutes {...this.props}/>
       </Provider>
