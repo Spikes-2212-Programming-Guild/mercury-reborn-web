b38b25608a8e9562c142334d89d2a049f033bedf
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-03 13:47:24 +0200

    added tactics to Homepage

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,14 +1,19 @@
 import React from "react"
-import { Label, Header } from "semantic-ui-react"
+import { Label, Header, List } from "semantic-ui-react"
 import { Link } from "react-router-dom"
 
 export default function Homepage (props) {
   return (
     <div>
       <Header as={"h1"}>Mercury</Header>
-      <Label size={"massive"} horizontal basic>
-        <Link to={"scouting/"}>Scout</Link>
-      </Label>
+      <List>
+        <List.Item>
+          <Label basic size={"massive"} horizontal><Link to={"/scouting"} className="scouting">Scouting</Link></Label>
+        </List.Item>
+        <List.Item>
+          <Label basic size={"massive"} horizontal><Link to={"/tactics"}>Tactics</Link></Label>
+        </List.Item>
+      </List>
     </div>
   )
 }
