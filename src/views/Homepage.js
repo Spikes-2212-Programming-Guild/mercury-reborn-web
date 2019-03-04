41fe7799bb66f3f543f5f1c368cd4883d7412c32
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-04 16:37:51 +0200

    added main menu

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,19 +1,20 @@
 import React from "react"
 import { Label, Header, List } from "semantic-ui-react"
 import { Link } from "react-router-dom"
+import ResetConf from "./scouting/ScoutingHomepage"
 
 export default function Homepage (props) {
   return (
     <div>
-      <Header as={"h1"}>Mercury</Header>
       <List>
         <List.Item>
-          <Label basic size={"massive"} horizontal><Link to={"/scouting"} className="scouting">Scouting</Link></Label>
+          <Label basic size={"big"} horizontal><Link to={"/scouting"} className="scouting">Scouting</Link></Label>
         </List.Item>
         <List.Item>
-          <Label basic size={"massive"} horizontal><Link to={"/tactics"}>Tactics</Link></Label>
+          <Label basic size={"big"} horizontal><Link to={"/tactics"}>Tactics</Link></Label>
         </List.Item>
       </List>
+      <ResetConf />
     </div>
   )
 }
--- src/views/scouting/ScoutingHomepage.js
+++ src/views/scouting/ScoutingHomepage.js
@@ -2,7 +2,6 @@ import React from "react"
 
 import {Link} from "react-router-dom"
 import {Label, List} from "semantic-ui-react"
-import ResetConf from "../ResetConf"
 
 class ScoutingHomepage extends React.Component {
   render() {
@@ -11,16 +10,15 @@ class ScoutingHomepage extends React.Component {
         <h1>Choose Your Scouting...</h1>
         <List>
           <List.Item>
-            <Label basic size={"massive"} horizontal><Link to={"/scouting/field/matches"} className="scouting">Scout Field</Link></Label>
+            <Label basic size={"big"} horizontal><Link to={"/scouting/field/matches"} className="scouting">Scout Field</Link></Label>
           </List.Item>
           <List.Item>
-            <Label basic size={"massive"} horizontal><Link to={"/scouting/spectator/matches/"}>Scout Spectator</Link></Label>
+            <Label basic size={"big"} horizontal><Link to={"/scouting/spectator/matches/"}>Scout Spectator</Link></Label>
           </List.Item>
           <List.Item>
-            <Label basic size={"massive"} horizontal><Link to={"/scouting/pit/teams"}>Scout Pits</Link></Label>
+            <Label basic size={"big"} horizontal><Link to={"/scouting/pit/teams"}>Scout Pits</Link></Label>
           </List.Item>
         </List>
-        <ResetConf />
       </div>
     )
   }
