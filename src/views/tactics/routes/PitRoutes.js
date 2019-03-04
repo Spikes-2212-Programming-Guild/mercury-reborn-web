749feb0b378801d6c81735f9c4f24c227871af62
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-04 16:16:21 +0200

    added PitTeamsMenu

--- src/views/tactics/routes/PitRoutes.js
+++ src/views/tactics/routes/PitRoutes.js
@@ -1,5 +1,27 @@
 import React from "react"
+import { Route } from "react-router-dom"
+import TeamsContainer from "../../../containers/teams-container"
+import { fetchSavedPitScoutingTeams } from "../../../connectors/mercury-api-connector"
+import { Provider } from "unstated"
+import PitTeamsMenu from "../pit/PitTeamsMenu"
 
-export function PitRoutes(props) {
+export class PitRoutes extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      teamsContainer: new TeamsContainer()
+    }
+
+    fetchSavedPitScoutingTeams().then(teams => this.state.teamsContainer.setTeams(teams))
+  }
+
+  render () {
+    return (
+      <Provider inject={[this.state.teamsContainer]}>
+        <Route path={`${this.props.match.path}/pit`} exact component={PitTeamsMenu}/>
+      </Provider>
+    )
+  }
 
 }
