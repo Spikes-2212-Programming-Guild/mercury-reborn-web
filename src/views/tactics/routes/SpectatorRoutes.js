100adc4e7be598252b2bd994bbf0debd611893e3
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-05 12:36:23 +0200

    implemented SpectatorRoutes

--- src/views/tactics/routes/SpectatorRoutes.js
+++ src/views/tactics/routes/SpectatorRoutes.js
@@ -1,5 +1,30 @@
 import React from "react"
+import { fetchSavedSpectatorMatches } from "../../../connectors/mercury-api-connector"
+import { Provider } from "unstated"
+import { Route } from "react-router-dom"
+import PitTeamsMenu from "../pit/PitTeamsMenu"
+import MatchesContainer from "../../../containers/matches-container"
 
-export function SpectatorRoutes(props) {
+export class SpectatorRoutes extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      matchesContainer: new MatchesContainer()
+    }
+
+    fetchSavedSpectatorMatches().then(matches => this.state.matchesContainer.setMatches(
+      matches.map(match => {return {name:match}})
+    ))
+  }
+
+  render () {
+    return (
+      <Provider inject={[this.state.matchesContainer]}>
+        <Route path={`${this.props.match.path}/spectator`} exact component={PitTeamsMenu}/>
+        <Route path={`${this.props.match.path}/spectator/:match`} component={}/>
+      </Provider>
+    )
+  }
 
 }
