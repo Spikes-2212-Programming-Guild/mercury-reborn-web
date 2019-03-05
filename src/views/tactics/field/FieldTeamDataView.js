470f39b5bae480ef2497b923ec20f54b2c32c344
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-03-05 22:25:23 +0200

    added DataDisplayPage

--- src/views/tactics/field/FieldTeamDataView.js
+++ src/views/tactics/field/FieldTeamDataView.js
@@ -1,23 +1,39 @@
 import React from "react"
 import { fetchFieldTeamSummary } from "../../../connectors/mercury-api-connector"
+import DataDisplayPage from "../../../components/display-data/questions/DataDisplayPage"
+import * as _ from "lodash"
 
 export default class FieldTeamDataView extends React.Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      summary: ""
+      summary: {},
+      matches: []
     }
 
     const {team_id} = this.props.match.params
 
-    fetchFieldTeamSummary(team_id).then(summary => this.setState({summary}))
+    fetchFieldTeamSummary(team_id).then(summary => {
+      const {matches} = summary
+      delete summary.matches
+      this.setState({summary, matches})
+    })
   }
 
-  render() {
-    return <div>
-      {JSON.stringify(this.state.summary)}
-    </div>
+  render () {
+    return (
+      <div>
+        {
+          _.map(this.state.summary, (questions, section) => (
+            <div key={section}>
+              <h3>{section}</h3>
+              <DataDisplayPage matches={this.state.matches} questions={questions}/>
+            </div>
+          ))
+        }
+      </div>
+    )
   }
 
 }
