96caa25ea3039e28ce1b1a6227e5407dc0880195
Ubuntu <ubuntu@ip-172-31-41-48.eu-central-1.compute.internal>
2019-03-06 00:58:19 +0000

    added api routes

--- src/connectors/mercury-api-connector.js
+++ src/connectors/mercury-api-connector.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 async function fetchConfigOption(option) {
-  return (await axios.get(`/config/${option}`)).data
+  return (await axios.get(`/api/config/${option}`)).data
 }
 
 export const fetchTBAKey = () => fetchConfigOption("tba-key")
@@ -12,45 +12,45 @@ export const fetchSpectatorForm = () => fetchConfigOption("spectator-form")
 
 
 export async function submitFieldForm (match) {
-  return await axios.post("/scouting/field/match/submit", {match})
+  return await axios.post("/api/scouting/field/match/submit", {match})
 }
 
 export async function submitPitForm(form) {
-  return await axios.post("/scouting/pit/team/submit", {form})
+  return await axios.post("/api/scouting/pit/team/submit", {form})
 }
 
 export async function submitSpectatorForm(match) {
-  return await axios.post("/scouting/spectator/match/submit", {match})
+  return await axios.post("/api/scouting/spectator/match/submit", {match})
 }
 
 export async function fetchSavedFieldMatches() {
-  return (await axios.get("/tactics/field/match-names")).data
+  return (await axios.get("/api/tactics/field/match-names")).data
 }
 
 export async function fetchSavedFieldTeams() {
-  return (await axios.get("/tactics/field/teams")).data
+  return (await axios.get("/api/tactics/field/teams")).data
 }
 
 export async function fetchSavedPitScoutingTeams() {
-  return (await axios.get("/tactics/pit/team-ids")).data
+  return (await axios.get("/api/tactics/pit/team-ids")).data
 }
 
 export async function fetchSavedSpectatorMatches() {
-  return (await axios.get("/tactics/spectator/match-names")).data
+  return (await axios.get("/api/tactics/spectator/match-names")).data
 }
 
 export async function fetchFieldMatch(team_id, match_name) {
-  return (await axios.get(`/tactics/field/match/${team_id}/${match_name}`)).data
+  return (await axios.get(`/api/tactics/field/match/${team_id}/${match_name}`)).data
 }
 
 export async function fetchPitScouting(team_id) {
-  return (await axios.get(`/tactics/pit/team/${team_id}`)).data
+  return (await axios.get(`/api/tactics/pit/team/${team_id}`)).data
 }
 
 export async function fetchSpectatorMatch(match_name) {
-  return (await axios.get(`/tactics/spectator/match/${match_name}`)).data
+  return (await axios.get(`/api/tactics/spectator/match/${match_name}`)).data
 }
 
 export async function fetchFieldTeamSummary(team_id) {
-  return (await axios.get(`/tactics/field/matches/${team_id}`)).data
+  return (await axios.get(`/api/tactics/field/matches/${team_id}`)).data
 }
