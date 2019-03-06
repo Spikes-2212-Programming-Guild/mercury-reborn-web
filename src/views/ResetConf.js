dddb0188cdd80e90e47a7ce5cb0f71efac70a2de
Ubuntu <ubuntu@ip-172-31-41-48.eu-central-1.compute.internal>
2019-03-06 12:11:05 +0000

    fixed from server

--- src/views/ResetConf.js
+++ src/views/ResetConf.js
@@ -1,10 +1,10 @@
 import React from "react"
-import { Link } from "semantic-ui-react"
+import { Link, Label } from "semantic-ui-react"
 
 export default class Reset extends React.Component {
   render() {
     return (
-      <a href="/" className="conf-reset" onClick={() => localStorage.clear()}> Clear Storage</a>
+      <Label href="/" basic className="conf-reset" onClick={() => localStorage.clear()}>Reset Forms</Label>
     )
   }
 }
