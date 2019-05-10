3d81f08fc5606c227154b86ec4ad8356c1bf3b0c
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-05-10 20:36:57 +0300

    refactored FormTable

--- src/components/display-data/FormTable.js
+++ src/components/display-data/FormTable.js
@@ -1,25 +1,24 @@
 import React from "react"
-import { Header, Table } from "semantic-ui-react"
+import { Table } from "semantic-ui-react"
 import SimpleTable from "./SimpleTable"
 import * as _ from "lodash"
 
-class FormTable extends React.Component {
-  render () {
-    const {Row, Cell, Header, HeaderCell} = Table
-    delete this.props.data["team_id"]
-    delete this.props.data["name"]
-    const {data} = this.props
-    return (
-      _.map(data, (questions, category) =>
-        (
-          <div>
-            <Header as={"h3"}>{category}</Header>
-            <SimpleTable data={questions}/>
-          </div>
-        )
+function FormTable (props) {
+  console.log("props are", props)
+  const {Header} = Table
+  delete props.data["team_id"]
+  delete props.data["name"]
+  const {data} = props
+  return (
+    _.map(data, (questions, category) =>
+      (
+        <div>
+          <Header as={"h3"}>{category}</Header>
+          <SimpleTable data={questions}/>
+        </div>
       )
     )
-  }
+  )
 }
 
 export default FormTable
