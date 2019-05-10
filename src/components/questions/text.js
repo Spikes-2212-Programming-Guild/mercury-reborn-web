8794ddeb7663b2a60d4c7a842119c656e1eef7d2
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-05-10 20:43:17 +0300

    refactored text component

--- src/components/questions/text.js
+++ src/components/questions/text.js
@@ -2,33 +2,18 @@ import React from "react"
 import { TextArea } from "semantic-ui-react"
 import propTypes from "prop-types"
 
-class Text extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      text: ""
-    }
-  }
+function Text(props){
 
-  static propTypes = {
-    consumer: propTypes.func,
-    supplier: propTypes.func
-  }
+  const {consumer, supplier} = props
 
-  handleChange = (e, data) => {
-    this.saveAll(data)
-  }
+  return (
+    <TextArea onChange={consumer}>{supplier()}</TextArea>
+  )
+}
 
-  saveAll = data => {
-    this.setState({text: data.value})
-    this.props.consumer(data.value)
-  }
-
-  render() {
-    return (
-      <TextArea onChange={this.handleChange}>{this.props.supplier()}</TextArea>
-    )
-  }
+Text.propTypes = {
+  consumer: propTypes.func,
+  supplier: propTypes.func
 }
 
 export default Text
