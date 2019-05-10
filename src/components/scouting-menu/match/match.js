3a6fc565bb9d625e26add0b6b883c4e2c1b79dfe
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-05-10 20:46:07 +0300

    refactored match

--- src/components/scouting-menu/match/match.js
+++ src/components/scouting-menu/match/match.js
@@ -5,68 +5,61 @@ import { withRouter } from "react-router-dom"
 import "./style.css"
 import MatchesContainer from "../../../containers/matches-container"
 
-class Match extends React.Component {
+function Match (props) {
 
-  constructor (props) {
-    super(props)
+  const scoutTeam = (matchName, team) => {
+    const newPath = `${props.parentURL}${matchName}/${team}`
+    props.history.push(newPath)
   }
 
-  scoutTeam(matchName, team) {
-    const newPath = `${this.props.parentURL}${matchName}/${team}`
-    this.props.history.push(newPath)
-  }
+  const {match} = props
+  const {name} = match.params
+  return (
+    <Subscribe to={[MatchesContainer]}>
+      {container => {
+        const matchData = container.getMatch(name)
+        console.log("match data")
+        console.log(matchData)
+        if (matchData) {
+          const {blue, red} = matchData
 
-
-  render() {
-    const {match} = this.props
-    const {name} = match.params
-    return (
-      <Subscribe to={[MatchesContainer]}>
-        {container => {
-          const matchData = container.getMatch(name)
-          console.log('match data')
-          console.log(matchData)
-          if (matchData) {
-            const {blue, red} = matchData
-
-            const joined = []
-            for (let i = 0; i < blue.length; i++) {
-              joined.push({blue: blue[i], red: red[i]});
-            }
-            return (
-              <div>
-                <Grid centered padded columns={2}>
-                  <Grid.Column>
-                    <Button.Group color={"blue"} inverted vertical fluid>
-                      {blue.map(team => (
-                        <Button
-                          class_name="team_button"
-                          onClick={() => this.scoutTeam(name, team)}>
-                          {team}
-                        </Button>
-                      ))}
-                    </Button.Group>
-                  </Grid.Column>
-                  <Grid.Column>
-                    <Button.Group color={"red"} inverted vertical fluid>
-                      {red.map(team => (
-                        <Button
-                          class_name="team_button"
-                          onClick={() => this.scoutTeam(name, team)}>
-                          {team}
-                        </Button>
-                      ))}
-                    </Button.Group>
-                  </Grid.Column>
-                </Grid>
-              </div>
-            )
+          const joined = []
+          for (let i = 0; i < blue.length; i++) {
+            joined.push({blue: blue[i], red: red[i]})
           }
-          return ""
-        }}
-      </Subscribe>
-    )
-  }
+          return (
+            <div>
+              <Grid centered padded columns={2}>
+                <Grid.Column>
+                  <Button.Group color={"blue"} inverted vertical fluid>
+                    {blue.map(team => (
+                      <Button
+                        class_name="team_button"
+                        onClick={() => scoutTeam(name, team)}>
+                        {team}
+                      </Button>
+                    ))}
+                  </Button.Group>
+                </Grid.Column>
+                <Grid.Column>
+                  <Button.Group color={"red"} inverted vertical fluid>
+                    {red.map(team => (
+                      <Button
+                        class_name="team_button"
+                        onClick={() => scoutTeam(name, team)}>
+                        {team}
+                      </Button>
+                    ))}
+                  </Button.Group>
+                </Grid.Column>
+              </Grid>
+            </div>
+          )
+        }
+        return ""
+      }}
+    </Subscribe>
+  )
 }
 
 export default withRouter(Match)
