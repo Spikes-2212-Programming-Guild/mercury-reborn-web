4b3d219e1820ff16a6ba31daf026119ceaa6f1f3
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-05-14 11:03:20 +0300

    refactored ResetConf to a functional component

--- src/views/ResetConf.js
+++ src/views/ResetConf.js
@@ -1,10 +1,9 @@
 import React from "react"
 import { Link } from "semantic-ui-react"
 
-export default class Reset extends React.Component {
-  render() {
-    return (
-      <a href="/" className="conf-reset" onClick={() => localStorage.clear()}> Clear Storage</a>
-    )
-  }
+export default () => {
+  return (
+    <a href="/" className="conf-reset" onClick={() => localStorage.clear()}> Clear Storage</a>
+  )
 }
+
