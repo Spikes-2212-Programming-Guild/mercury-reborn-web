841525751c817f32e5285b1686af783af570fca4
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-05-15 11:21:21 +0300

    refactored props in TeamsList

--- src/components/TeamsList.js
+++ src/components/TeamsList.js
@@ -3,11 +3,11 @@ import { withRouter } from "react-router-dom"
 import propTypes from "prop-types"
 import { List } from "semantic-ui-react"
 
-function TeamsList (props) {
+function TeamsList ({history, teams, parentURL}) {
   return (
     <List vertical selection relaxed divided size={"huge"} fluid>
-      {props.teams.map((team, index) => (
-        <List.Item key={index} onClick={() => props.history.push(`${props.parentURL}/${team}`)}>
+      {teams.map((team, index) => (
+        <List.Item key={index} onClick={() => history.push(`${parentURL}/${team}`)}>
           {team}
         </List.Item>
       ))}
