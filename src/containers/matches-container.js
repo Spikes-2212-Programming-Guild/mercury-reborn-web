f553b2d8c589e63a34fce68600d052c0d3e928a0
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-05-15 11:23:02 +0300

    refactored matches container to unstated-next

--- src/containers/matches-container.js
+++ src/containers/matches-container.js
@@ -1,22 +1,12 @@
-import { Container } from "unstated"
+import React, {useState} from "react"
+import {createContainer} from "unstated-next"
 import * as _ from "lodash"
 
-export default class MatchesContainer extends Container {
 
-  constructor () {
-    super()
-    this.state = {
-      matches: []
-    }
-  }
-
-  setMatches(matches) {
-    return this.setState({matches}).then(() => console.log(this.state))
-  }
-
-  getMatch(name) {
-    console.log(this.state.matches)
-    return _.filter(this.state.matches, match => match.name === name)[0]
-  }
-
+const MatchesContainer = () => {
+  const [matches, setMatches] = useState([])
+  const getMatch = (name) => _.filter(matches, match => match.name === name)[0]
+  return {matches, setMatches, getMatch}
 }
+
+export default createContainer(MatchesContainer)
