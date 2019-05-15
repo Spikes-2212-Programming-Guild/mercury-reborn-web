01d9c35127116d1d5a3a751d44fa93acd1ac7458
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-05-15 11:22:25 +0300

    made team and match menus work with hooks

--- src/components/scouting-menu/MatchesMenu.js
+++ src/components/scouting-menu/MatchesMenu.js
@@ -1,15 +1,8 @@
-import { Subscribe } from "unstated"
 import { MatchList } from "../match-list"
 import React from "react"
 import MatchesContainer from "../../containers/matches-container"
 
 export default function MatchesMenu (props) {
-  return (
-    <Subscribe to={[MatchesContainer]}>
-      {container => {
-        console.log(container.state)
-        return <MatchList {...props} matches={container.state.matches}/>
-      }}
-    </Subscribe>
-  )
+  const container = MatchesContainer.useContainer()
+  return <MatchList {...props} matches={container.matches}/>
 }
--- src/components/scouting-menu/TeamsMenu.js
+++ src/components/scouting-menu/TeamsMenu.js
@@ -1,14 +1,9 @@
-import { Subscribe } from "unstated"
 import TeamsList from "../TeamsList"
 import React from "react"
 import TeamsContainer from "../../containers/teams-container"
 
 export default function TeamsMenu (props) {
-  return (
-    <Subscribe to={[TeamsContainer]}>
-      {container => {
-        return <TeamsList {...props} teams={container.state.teams}/>
-      }}
-    </Subscribe>
-  )
+  const container = TeamsContainer.useContainer()
+  return <TeamsList {...props} teams={container.teams}/>
+
 }
